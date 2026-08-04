f86cf80dcaca35d31cf7e034234feda45146be58
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2026-08-04 17:43:56 -0400

    Write buildTree() method in tree class Implement prettyPrint() method to print entire BST

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 //entry point
 import Tree from "./tree.js";
 
-const binaryTree = new Tree([3, 9, 6, 5, 1, 4, 2, 7, 10, 8]);
+const binaryTree = new Tree([3, 3, 9, 6, 5, 1, 4, 2, 7, 10, 8]);
+
+//print node structure from root
+console.log(binaryTree.root);
+
+//print entire tree
+binaryTree.prettyPrint();
--- src/tree.js
+++ src/tree.js
@@ -8,48 +8,92 @@ class Node{
 
 export default class Tree{
     constructor(arr){
-        this.arr = arr;
-        this.root = null; //use return value of buildTree()
+        this.root = this.#buildTree(arr);
     }
 
-    buildTree(array){
+    //method to print entire BST to console
+    prettyPrint(node = this.root, prefix = '', isLeft = true){
+        if (node === null || node === undefined) {
+            return;
+        }
+
+        this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+        this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    };
+
+    #buildTree(arr){
+        
         //remove duplicate values first
+        const noDupes = [...new Set(arr)];
+        
         //sort the array next 
+        const sortedArray = noDupes.sort((a, b) => a - b);
+
         //write recursive function to create BST
-        //return the level-0 root node
+        function sortedToBST(arr, start, end){
+            //create base case for recursion
+            if(start > end) return null;
 
+            let mid = start + Math.floor((end - start) / 2);
+            let root = new Node(arr[mid]);
 
-        //make this function private 
+            root.left = sortedToBST(arr, start, mid - 1);
+            root.right = sortedToBST(arr, mid + 1, end);
+
+            return root;
+        }
+
+        return sortedToBST(sortedArray, 0, sortedArray.length - 1);
     }
 
     includes(value){
         //traverse through tree to find value
+
         //if value... true
+
         //if no value... false
+
     }
 
     insert(value){
-        //insert a new node in pre-existing binary tree.
+        //FIRST CHECK IF THE VALUE ALREADY EXISTS. IF IT DOES. RETURN EARLY.
+
         //loop through the tree. start at the root.
+
         //compare the "value" to the node being iterated on; if it's smaller, go left, if bigger, go right.
+
         //continue until you find a "null" spot where the node can be placed.
+
         //change that null value to the "value"
+
     }
 
     deleteItem(value){
+        //MAYBE USE INCLUDES() METHOD TO FIRST CHECK IF THE VALUE EVEN EXISTS? IF IT DOESN'T, RETURN EARLY.
+
         //pass in a node to be deleted from the binary tree
+
         //loop through the binary tree
+
+        //compare value to currentNode. If smaller, go left. If bigger, go right. 
+
+        
         //multiple niche cases!!:
+
         //if NO children: simply remove the node.
+
         //if ONE child, remove passed in node and connects it's parent directly to it's one child in the same spot 
+
         //if TWO children, ...??? how to choose between which child takes its spot? <---- read up on this 
+
     }
 }
 
 
 //FUNCTION FOR GENERATING STRUCTURED LOOK AT TREE FORMAT IN CONSOLE
 
-// const prettyPrint = (node, prefix = '', isLeft = true) => {
+// prettyPrint(node, prefix = '', isLeft = true){
 //     if (node === null || node === undefined) {
 //         return;
 //     }
@@ -57,4 +101,4 @@ export default class Tree{
 //     prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
 //     console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
 //     prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-// }
+// };
